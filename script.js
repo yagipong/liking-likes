83ce32bc396cff04fd74cb091769306aef29aa3c
Yaw Agipong <yagipong@outlook.com>
2022-06-29 15:13:09 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var countup = 3
+var countupElement = document.querySelector("#countup");
+
+function add(){
+    countup++;
+    countupElement.innerText = countup + " like(s)";
+}
+
+var countup1 = 9
+var countup1Element = document.querySelector("#countup1");
+
+function add1(){
+    countup1++;
+    countup1Element.innerText = countup1 + " like(s)";
+}
+
+var countup2 = 12
+var countup2Element = document.querySelector("#countup2");
+
+function add2(){
+    countup2++;
+    countup2Element.innerText = countup2 + " like(s)";
+}
+
+var countup3 = 9
+var countup3Element = document.querySelector("#countup3");
+
+function add3(){
+    countup3++;
+    countup3Element.innerText = countup3 + " like(s)";
+}
